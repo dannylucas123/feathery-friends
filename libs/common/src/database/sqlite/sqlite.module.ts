a27c010700cadc915e57d44d06f5b3a63232d8ac
Danny <danny.lucas@live.nl>
2023-08-19 06:21:07 +0200

    fixed startup bugs

--- libs/common/src/database/sqlite/sqlite.module.ts
+++ libs/common/src/database/sqlite/sqlite.module.ts
@@ -5,7 +5,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      imports: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'sqlite',
         database: configService.get<string>('SQLITE_DATABASE_NAME'),
